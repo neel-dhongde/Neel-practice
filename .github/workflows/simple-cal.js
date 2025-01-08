f05d4bc64c5fcc03a50b3b093778dfe6a2500865
Neel Dhongde <neel.dhongde23@gmail.com>
2025-01-08 09:50:43 +0000

    Added simple calc

---
+++ .github/workflows/simple-cal.js
@@ -0,0 +1,12 @@
+// simple-calculator.js
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log("Addition of 5 and 3: " + add(5, 3));
+console.log("Subtraction of 5 and 3: " + subtract(5, 3));
